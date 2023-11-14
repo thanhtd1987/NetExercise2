7cc1f8cd78adbc10648e5e4cf0f7e2ca3d4bfd43
thanhTD <thanhtd1987@gmail.com>
2023-11-14 08:29:36 +0700

    Add first functions and test

---
+++ NetExercise.Test/Basic/Exercise01Test.cs
@@ -0,0 +1,22 @@
+﻿using NetExercise.Basic;
+using NUnit.Framework;
+namespace NetExercise.Test.Basic
+{
+    public class Exercise01Test
+    {
+        [TestCase(12, 28, 4)]
+        [TestCase(42, 28, 14)]
+        [TestCase(10, 1, 1)]
+        public void Test_Ucln_Ok(int a, int b, int result)
+        {
+            Assert.AreEqual(Exercise01.Ucln(a, b), result);
+        }
+
+        [TestCase(12, 28, 84)]
+        [TestCase(42, 35, 210)]
+        public void Test_Bcnn_Ok(int a, int b, int result)
+        {
+            Assert.AreEqual(Exercise01.Bcnn(a, b), result);
+        }
+    }
+}
---
+++ NetExercise/Basic/Exercise01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace NetExercise.Basic
+{
+    public class Exercise01
+    {
+        public static int Ucln(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                {
+                    a %= b;
+                }
+                else
+                {
+                    b %= a;
+                }
+            }
+            return a + b;
+        }
+
+        public static int Bcnn(int a, int b)
+        {
+            return a * b / Ucln(a, b);
+        }
+    }
+
+}
